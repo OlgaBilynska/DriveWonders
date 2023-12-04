43a9deff3e7af7cdcab073fc7bab351c71db6ab1
OlgaBilynska <olgabilinskaya@gmail.com>
2023-12-04 02:30:01 +0200

    consoles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,25 @@
-import { Suspense } from "react";
+import { Suspense } from 'react';
+import { lazy } from 'react';
 
-import { Route, Routes, useLocation } from "react-router-dom";
-import Loader from "../Loader";
-import { AppWrapperStyled } from "./App.styled.js";
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Loader from '../Loader';
+import { AppWrapperStyled } from './App.styled.js';
 
-import { ToastContainer } from "react-toastify";
-import ErrorPage from "../../pages/ErrorPage/ErrorPage.jsx";
-import ContentWrapper from "../ContentWrapper/ContentWrapper.jsx";
-import carBackground from "../../assets/car_background.jpg";
+import { ToastContainer } from 'react-toastify';
+import ErrorPage from '../../pages/ErrorPage/ErrorPage.jsx';
+import ContentWrapper from '../ContentWrapper/ContentWrapper.jsx';
+import carBackground from '../../assets/car_background.jpg';
 
-// const HomePage = lazy(() => import("../../pages/HomePage"));
-// const CatalogPage = lazy(() => import("../../pages/CatalogPage"));
-// const FavoritePage = lazy(() => import("../../pages/FavoritePage"));
-
-import HomePage from "../../pages/HomePage/HomePage.jsx";
-import CatalogPage from "../../pages/CatalogPage/CatalogPage.jsx";
-import FavoritePage from "../../pages/FavoritePage/FavoritePage.jsx";
+const HomePage = lazy(() => import('../../pages/HomePage'));
+const CatalogPage = lazy(() => import('../../pages/CatalogPage'));
+const FavoritePage = lazy(() => import('../../pages/FavoritePage'));
 
 function App() {
   const location = useLocation();
 
   let backgroundImage = null;
 
-  // if (location.pathname === "/") {
-  //   return <Navigate to="/" />;
-  // }
-
-  if (location.pathname === "/") {
+  if (location.pathname === '/') {
     backgroundImage = `url(${carBackground})`;
   }
 
